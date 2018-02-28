22dc479385326ec3e74cd03779d572422d4d7fbb
dmitri.gorchanin <dmitri.gorchanin@MrBrown.local>
2018-02-28 12:22:49 +0300

    my first arversome result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  var res = 0;
+    
+  arr = [2,3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]
+
+  for (var i = 0, length = arr.length; i<length;i++){
+      if (base%arr[i]===0){
+          count = arr[i];
+      }
+  }
+  for (var i = count; i<number; i=i*count){
+      res=res+Math.floor(number/i);
+  }
+
+  return (res);
+  }
